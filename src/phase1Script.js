9a22da57f5de711f85e419c9a91eb70c4ad56718
moustafa2121 <moustafa2121@gmail.com>
2023-12-18 02:07:07 +0400

    cleaning up
    - moved date to the right instead of being above image
    - removed card BG into a more standard bg
    - tweeking

--- src/phase1Script.js
+++ src/phase1Script.js
@@ -7,32 +7,35 @@ import { throttle } from 'lodash';
 
 //given an entry type (i.e. RE for reddit), it returns specific
 //format to display data for each entry (e.g. icon, color, others)
-function getCardDesign(entryType, epoch) {
+function getCardDesign(entryType, epoch, img) {
+    const base = { gridTemplate: img ? "0% 70% auto":"0% auto auto",
+                date:formatDateFromEpoch(epoch, entryType)}
+    //todo: remove the bg values, we are not using them
     if (entryType === "RE")
-      return {icon:"3.png", bg:'repeating-linear-gradient(-30deg, rgba(255, 165, 0, 0.1), rgba(255, 165, 0, 0.1) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://www.reddit.com/", date:formatDateFromEpoch(epoch, entryType)}
+      return {...base, icon:"3.png", bg:'repeating-linear-gradient(-30deg, rgba(255, 165, 0, 0.1), rgba(255, 165, 0, 0.1) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://www.reddit.com/",}
     else if(entryType === "NW")
-      return {icon:"1.png", bg:'repeating-linear-gradient(-30deg, rgba(29, 161, 242, 0.2), rgba(29, 161, 242, 0.2) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://twitter.com/", date:formatDateFromEpoch(epoch, entryType)}
+      return {...base, icon:"1.png", bg:'repeating-linear-gradient(-30deg, rgba(29, 161, 242, 0.2), rgba(29, 161, 242, 0.2) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://twitter.com/",}
     else if (entryType === "ME")
-      return {icon:"2.png", bg:'repeating-linear-gradient(-30deg, rgba(100, 100, 100, 0.2), rgba(100, 100, 100, 0.2) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://knowyourmeme.com/", date:formatDateFromEpoch(epoch, entryType), borderRadius:"50%"}
+      return {...base, icon:"2.png", bg:'repeating-linear-gradient(-30deg, rgba(100, 100, 100, 0.2), rgba(100, 100, 100, 0.2) 3px, rgba(40,44,52,0.5) 2px, rgba(40,44,52,0.5) 10px)', link:"https://knowyourmeme.com/", borderRadius:"50%"}
 }
 
 //the component for the card (i.e. the card-like that displays each entry)
 function Card(props){
     //fetch data based on the entry type of the card
-    const cardDesign = getCardDesign(props.entryType, props.date)
+    const cardDesign = getCardDesign(props.entryType, props.date, props.img)
     return(
-        <article className="card" style={{ background: cardDesign.bg }}>
+    <article className="card" style={{ background: "#222224", gridTemplateColumns: `${cardDesign.gridTemplate}`}}>
         <a href={cardDesign.link} target="_blank">
             <img className="cardIcon" src={cardDesign.icon} alt="Avatar" style={{borderRadius:`${cardDesign.borderRadius}`}}></img>
         </a>
-        <div className="cardTitle">
-            <a target="_blank" href={props.url}><p>{props.title}</p></a>
-        </div>
-        <div className="cardRight">
+        <div className="cardLeft">
             <div className="cardDate">{cardDesign.date}</div>
-            {props.img ? <img className="cardImg" src={props.img} alt="Card Image"/> : ''}
+            <div className="cardTitle">
+                <a target="_blank" href={props.url}><p>{props.title}</p></a>
+            </div>
         </div>
-        </article>
+        {props.img ? <img className="cardImg" src={props.img} alt="Card Image"/> : ''}
+    </article>
     )
 }
 
